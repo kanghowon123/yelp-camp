9aa50c05039254109c421e17819d4e15384232f6
howon <kang05209@gmail.com>
2025-04-08 19:38:23 +0900

    데이터베이스 URL 수정

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoStore = require('connect-mongo');
 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 mongoose.connect(dbUrl);
 
 // 이 로직은 오류를 확인하고 오류 없이 제대로 열렸다면 연결 문구를 출력함
