6f1a862dbab568c4da516b6d61ead8052ec8c60f
howon <kang05209@gmail.com>
2025-04-08 20:12:52 +0900

    ssl 추가

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ const reviewRoutes = require('./routes/reviews');
 const MongoStore = require('connect-mongo');
 
 const dbUrl = process.env.DB_URL;
-mongoose.connect(dbUrl);
+mongoose.connect(dbUrl, {
+  ssl: true,
+});
 
 // 이 로직은 오류를 확인하고 오류 없이 제대로 열렸다면 연결 문구를 출력함
 const db = mongoose.connection;
